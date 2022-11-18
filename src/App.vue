f49c436a88f48ce03cde5e3a4f51436ae05fc1df
rima-celita.lewis <rima-celita.lewis@onelogic.de>
2022-11-18 14:39:10 +0100

    display list of all beer

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,38 @@
 <template>
-  <beer-info></beer-info>
+  <beer-info v-if="beers.length>0" :beers="beers" class="main-container"></beer-info>
 </template>
 
 <script>
 import BeerInfo from './components/BeerInfo'
-
+const axios = require('axios').default;
 export default {
   name: 'App',
   components: {
     BeerInfo
+  },
+  data(){
+    return {
+      beers: []
+    }
+  },
+  mounted() {
+   const response = this.getUser();
+    response.then(res => { this.beers = res.data})
+  },
+  methods: {
+     async getUser() {
+     return await axios.get('https://api.punkapi.com/v2/beers?brewed_before=11-2012&abv_gt=6');
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.main-container {
+  font-family: system-ui;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: #2c3e50;
 }
 </style>
--- src/components/BeerInfo.vue
+++ src/components/BeerInfo.vue
@@ -1,9 +1,69 @@
 <template>
-  <h1>Hi, i am here</h1>
+  <div>
+  <div class="beer-info-container" v-for="beer in beers" :key="beer.id">
+    <div>
+      <img class="beer-image" :src=beer.image_url>
+    </div>
+
+    <div class="beer-info">
+      <h1>{{beer.name}}</h1>
+      <h3>{{beer.tagline}}</h3>
+      <h4 class="description">Description: {{beer.description}}</h4>
+      <h4>Tips:{{beer.brewers_tips}}</h4>
+
+      <h4>Food Pairings:</h4>
+      <div>
+        <ul>
+          <li v-for="foodPairing in beer.food_pairing" :key="foodPairing">
+            {{foodPairing}}
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name:"BeerInfo"
+  name:"BeerInfo",
+  props: {
+    beers: Array
+  }
 }
 </script>
+
+<style lang="css" scoped>
+.beer-info-container {
+  display: flex;
+  height:500px;
+  margin-top: 50px;
+  margin-left:30%;
+  margin-right:30%;
+  background-color: red;
+}
+
+.beer-image {
+  margin-left: 20px;
+  margin-top: 20px;
+  margin-right: 20px;
+  height: 93%;
+  width: 150px;
+}
+
+.beer-info {
+  width: calc(100% - 150px);
+  padding: 60px 20px 60px 20px;
+  background-color: olive;
+  color: white;
+  text-align: left;
+}
+
+.description {
+  max-height: 50px;
+  height: 50px;
+  overflow: auto;
+  text-overflow: ellipsis;
+}
+</style>
